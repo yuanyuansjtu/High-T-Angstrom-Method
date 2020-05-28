e5de7115247501f4d5a8c92a7c608ffa7a7a8cd0
yuanyuansjtu <Ilovecode01>
2020-05-27 21:30:46 -0700

    add two python files to local directory so that they can run on pycharm without on Jupyter notebook

--- run_batch_process_batch_III_2_rec162_181_LB_Amax_high_variable_R0_T1_emissivity_sigma_s.py
+++ run_batch_process_batch_III_2_rec162_181_LB_Amax_high_variable_R0_T1_emissivity_sigma_s.py
@@ -36,13 +36,14 @@ from datetime import datetime
 # (1)result cache dump, results will be saved in this folder
 # (2)batch process information, the experimental condition spread sheet will be in this folder
 # (3)temperature cache dump, the radial averaged temperature picke file will be in this folder
-df_exp_condition_spreadsheet_filename = 'batch_process_results_April_29_no_LB_HYPC_00001_00007_variable_alpha_T1_e_R0.xlsx'
+
+df_exp_condition_spreadsheet_filename = 'batch_process_batch_III_2_rec162_181_LB_Amax_high_variable_R0_T1_emissivity_sigma_s.xlsx'
 data_directory = "C://Users//yuan//Desktop//Amgstrom_method//temperature data//" # this is the directory to save csv files, note each experiment must have its own folder, for example Rec-0000X should be-> data directory + Rec-0000X+ //Rec-0000X_01.csv
 code_directory = "G://My Drive//Current Projects//Angstrom Method//High Temperature Simulation//Jupyter Notebook//"
 
 num_cores = 8 # number of cores to run regression in parallel, note there is a num_core in the excel spreadsheet, it indicates the number of cores required to process the radial averaged temperture profile
 
-#parallel_regression_batch_experimental_results(df_exp_condition_spreadsheet_filename,data_directory,num_cores,code_directory)
+parallel_regression_batch_experimental_results(df_exp_condition_spreadsheet_filename,data_directory,num_cores,code_directory)
 
 joblib_output = pickle.load(open( code_directory+"result cache dump//regression_results_" + df_exp_condition_spreadsheet_filename, 'rb'))
 
